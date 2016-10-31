29bf3a59b7db6cef7723196f768da0fd193fce31
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2016-10-31 18:52:39 +0100

    remove PHP -> HTML
    yup, we lost conf in the process.

--- includes/codetop.php
+++
@@ -1,8 +0,0 @@
-		<link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
-        <!-- <link rel="stylesheet" href="css/prettify.css"> -->
-
-        <link rel="stylesheet" href="css/main.css">
-        <link rel="stylesheet" href="css/select2.css">
-
-        <script src="js/libs/modernizr-2.6.1-respond-1.1.0.min.js"></script>
